3dd394d606e49f84cb505332764af4591de2dc70
Lukas Bauer <bauer2001.lukas@gmail.com>
2019-05-13 13:05:52 +0200

    LendingComponent erstellt

---
+++ LibraryFrontend/_Frontend/src/components/LendingComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-container>
+    <b-row>
+        <b-col>
+          <div>
+            <h3>Customer</h3>
+            <input class="form-control" placeholder="Search Customer ID">
+          </div>
+        </b-col>
+        <b-col>
+
+            <b-dropdown  id="whitespace">
+              <b-dropdown-item>gay</b-dropdown-item>
+            </b-dropdown>
+        </b-col>
+    </b-row>
+    <b-row>
+        <b-col>
+          <b-button variant="primary">Send Customer ID</b-button>
+        </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+    export default {
+        name: "LendingComponent"
+    }
+</script>
+
+<style scoped>
+  #whitespace{
+      padding-top: 2.6em;
+  }
+</style>
---
+++ LibraryFrontend/_Frontend/src/pages/MainPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 class="display-4">LENDING</h1>
+    <lending-component></lending-component>
+  </div>
+
+</template>
+
+<script>
+    import LendingComponent from "../components/LendingComponent";
+    export default {
+        name: "MainPage",
+      components: {
+          "lending-component": LendingComponent
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>

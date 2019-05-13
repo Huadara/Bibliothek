4ca2bbb9106b2684bfa0aa8ec66106baa6cdc829
Huadara <a.salletmaier@outlook.at>
2019-05-13 11:27:35 +0200

    Assessment-Tool entpackt

---
+++ Assessment-Tool/AssessmentModel/AssessmentContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentModel
+{
+    public class AssessmentContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<PersonTask> PersonTasks { get; set; }
+    }
+}
